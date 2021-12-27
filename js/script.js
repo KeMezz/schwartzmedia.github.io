a1863d2123dc9a5220a151e1c98e5d01b33e2417
KeMezz <pnescene@icloud.com>
2021-12-27 15:53:30 +0900

    헤더 수정

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const header = document.querySelector("header");
-const logo = header.querySelector(".logo");
+const logo = header.querySelector(".header___logo");
+const menuIcon = header.querySelector(".menu___hamburger");
 
 const onScroll = () => {
-    console.log("scroll")
-    if (scrollY === 0) {
-        header.style.boxShadow = "none";
-    } else {
-        header.style.boxShadow = "0 5px 5px lightgray";
-    }
-}
+  console.log("scroll");
+  if (scrollY === 0) {
+    header.style.boxShadow = "none";
+    header.classList.remove("active");
+    logo.classList.remove("active");
+    menuIcon.classList.add("active");
+  } else {
+    header.style.boxShadow = "0 5px 5px lightgray";
+    header.classList.add("active");
+    logo.classList.add("active");
+    menuIcon.classList.remove("active");
+  }
+};
 
+onScroll();
 window.addEventListener("scroll", onScroll);
